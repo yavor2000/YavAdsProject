77946b3faa3d78c6de7eca7506f65a5f9905fba0
Yavor Mitev <yavor2000@yahoo.com>
2015-01-03 02:01:20 +0200

    filters works

--- app/js/app.js
+++ app/js/app.js
@@ -9,12 +9,16 @@ adsApp.config(['$routeProvider', function($routeProvider) {
         templateUrl: 'templates/home.html',
         controller: 'MainController'
       })
+      .when('/home/page=:page', {
+          templateUrl: 'templates/home.html',
+          controller: 'MainController'
+      })
       .when('/all-ads', {
-          templateUrl: 'templates/all-ads.html',
+          templateUrl: 'templates/home.html',
           controller: 'MainController'
       })
       .when('/all-ads/page=:page', {
-          templateUrl: 'templates/all-ads.html',
+          templateUrl: 'templates/home.html',
           controller: 'MainController'
       })
       .when('/register', {
@@ -25,44 +29,7 @@ adsApp.config(['$routeProvider', function($routeProvider) {
           templateUrl: 'templates/login.html',
           controller: 'MainController'
       })
-      .otherwise({redirectTo: '/all-ads'});
+      .otherwise({redirectTo: '/home'});
 }]);
 
-adsApp.controller('MainController', function($scope, $location, $routeParams, mainData) {
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-        $location.path('/all-ads/page='+pageNo);
-    };
-
-    $scope.currentPage = 1;
-    $scope.totalItems = 99999999; // will be updated on ads load but of not set will cause setPage to 1
-    $scope.maxSize = 5; // number of pages in pagination control
-    $scope.itemsPerPage = 5;
-
-    if ($routeParams.page && $routeParams.page>0 ) {
-        $scope.currentPage = $routeParams.page;
-    }
-
-    $scope.pageChanged = function() {
-        console.log('Page changed to: ' + $scope.currentPage);
-        $location.path('/all-ads/page='+$scope.currentPage);
-    };
-
-    mainData.getAllAds($scope.itemsPerPage, $scope.currentPage, function(resp) {
-        $scope.data = resp;
-        $scope.totalItems = $scope.data.numItems;
-    });
-    mainData.getAllTowns(function(resp) {
-        $scope.towns = resp;
-    });
-    mainData.getAllCategories(function(resp) {
-        $scope.categories = resp;
-    });
-
-    //$scope.$watch('currentPage + itemsPerPage', function() {
-    //    var begin = (($scope.currentPage - 1) * $scope.itemsPerPage)
-    //        , end = begin + $scope.itemsPerPage;
-    //    //$scope.filteredTodos = $scope.todos.slice(begin, end);
-    //});
-});
 
--- app/js/services/mainPage.js
+++ app/js/services/mainPage.js
@@ -1,19 +1,23 @@
 /**
  * Created by Yavor on 31.12.2014 г..
  */
-adsApp.factory('mainData', function ($http, $log) {
+adsApp.factory('mainData', function ($http, $log, $rootScope) {
     return {
         getAllAds: function (pageSize, startPage, success) {
+            $rootScope.$broadcast('isLoading', true);
             $http({method:'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads?PageSize='+pageSize+'&StartPage='+startPage})
                 .success(function(data) { //data, status, headers, config
                 // this callback will be called asynchronously
                 // when the response is available
                     success(data);
-            })
+                })
                 .error(function(data) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
                     $log.warn(data);
+                })
+                .finally(function(){
+                    $rootScope.$broadcast('isLoading', false);
                 });
         },
         getAllTowns: function (success) {
@@ -40,6 +44,23 @@ adsApp.factory('mainData', function ($http, $log) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
                     $log.warn(data);
+                });
+        },
+        getAdsFilter: function (townId, categoryId, success) {
+            $rootScope.$broadcast('isLoading', true);
+            $http({method:'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads?townid='+townId+'&categoryid='+categoryId})
+                .success(function(data) { //data, status, headers, config
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    success(data);
+                })
+                .error(function(data) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    $log.warn(data);
+                })
+                .finally(function () {
+                    $rootScope.$broadcast('isLoading', false);
                 });
         }
     }
